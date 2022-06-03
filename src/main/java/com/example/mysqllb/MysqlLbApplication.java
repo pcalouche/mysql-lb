59be3a876f02525932965a8a1ff5b73815df6c78
Philip Calouche <philip.calouche@energyhub.net>
2022-06-03 15:41:38 -0400

    initial commit

---
+++ src/main/java/com/example/mysqllb/MysqlLbApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqllb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlLbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlLbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysqllb/MysqlLbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqllb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlLbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
